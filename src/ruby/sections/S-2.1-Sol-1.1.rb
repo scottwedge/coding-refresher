f121f579d45ec1d7a363a72c6d2a4ea891342ce9
BalaManohar.B <bala@test_user@pramata.com>
2018-12-05 23:55:44 +0530

    Section 2.1

---
+++ src/ruby/sections/S-2.1-Sol-1.1.rb
@@ -0,0 +1,12 @@
+def insertion_sort(arr)
+  1.upto(arr.length) do |index|
+    key = arr[index]
+    pos = index - 1
+    while pos >= 0 && key <= arr[pos]
+      arr[pos + 1] = arr[pos]
+      pos = pos - 1
+    end
+    arr[pos + 1] = key
+  end
+  arr
+end
