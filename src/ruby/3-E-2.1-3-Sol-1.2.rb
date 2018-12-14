97d2442cbe2e8e7528fb4dab5b127cad297acf7a
BalaManohar.B <balamanoharb@gmail.com>
2018-12-15 00:01:48 +0530

    Day 48

--- src/ruby/3-E-2.1-3-Sol-1.2.rb
+++ src/ruby/3-E-2.1-3-Sol-1.2.rb
@@ -2,7 +2,9 @@
 # have to remember that all recursion follows this format
 # base case
 # a repition of work
-# like the insertion sort i can go from 0 to length of the array and 
+# like the insertion sort I can go from 0 to length of the array or
+# from the length of the array down to 0
+# it's all the same anyway
 def linear_search(arr, key, pos = 0)
   return pos if key == arr[pos]
   return linear_search(arr, key, pos + 1) if pos < arr.length
