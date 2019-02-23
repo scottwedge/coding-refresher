4be0d834d69eb0897fd12913e15c04a064dac898
BalaManohar.B <balamanoharb@gmail.com>
2019-02-23 20:31:12 +0530

    recursion problems

---
+++ src/cs106b/python/backtracking/print_all_decimal.py
@@ -0,0 +1,11 @@
+def print_all_decimal(n):
+  helper(n, "")
+
+def helper(n, s):
+  if(len(s) == n):
+    print(s)
+  else:
+    for i in range(10):
+      helper(n, s + str(i))
+
+print_all_decimal(10)
