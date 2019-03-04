6da8a9c7a7a2c33921643ba8e3522ac90c5e67f5
BalaManohar.B <balamanoharb@gmail.com>
2019-03-04 22:49:53 +0530

    dice roll backtrack problem

---
+++ src/cs106b/python/backtracking/collect_all_dice_rolls.py
@@ -0,0 +1,18 @@
+# here the assumption is the user will give "n"
+# the function has to print all dice rolls possible for "n" dices
+
+def collect_all_dice_rolls(n):
+  result_set = []
+  helper(n, [], result_set)
+  return result_set
+
+def helper(n, roll_set, result_set):
+  if n == 0:
+    result_set.append(list(roll_set))
+  else:
+    for i in range(1,7):
+      roll_set.append(i)
+      helper(n - 1, roll_set, result_set)
+      roll_set.pop()
+
+print(collect_all_dice_rolls(3))
